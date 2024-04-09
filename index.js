d3261f91964645001282748cdd5dd8c9f0d53cba
Alex H <alex.holder@bigfootds.com>
2024-04-09 19:55:52 +1000

    feat!: generate random pokemon and log it

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log("Terminal app is running!");
 
+const pokemon = require('pokemon');
+
+let randomPokemon = pokemon.random();
+
+console.log(`Today's lucky Pokemon is ${randomPokemon}`);
 
 console.log("Bye bye, terminal app finished!");
 
