9033439a412d214443a2f7ebb82ac178887ac44d
Alex H <alex.holder@bigfootds.com>
2024-04-11 19:37:47 +1000

    feat: repeatable user input

--- index.js
+++ index.js
@@ -7,6 +7,64 @@ const pokemonPrinterFile = require("./pokemonPrinter");
 
 // import blahblah from 'dotenv';
 
+
+
+// Style 1 for setting up prompt-sync:
+var prompt = require('prompt-sync')({sigint: true});
+
+// Style 2 for setting up prompt-sync:
+let promptPackage = require('prompt-sync');
+let promptInstance = promptPackage({sigint: true});
+
+
+//
+// get input from the user.
+//
+// var n = prompt('What number of Pokemon do you want to see?');
+
+
+let userWantsToExit = false;
+
+do {
+
+	let n = parseFloat(prompt("What number of Pokemon do you want to see?"));
+	console.log("You entered " + n);
+
+	let userInputToExit = prompt("Would you like to try again?");
+
+	if (userInputToExit == "y"){
+		userWantsToExit = false;
+	} else {
+		userWantsToExit = true;
+	}
+
+} while (userWantsToExit == false);
+
+
+
+
+// console.log("User entered: " + n);
+// console.log(typeof(n));
+
+// let nAsNumber = parseFloat(n);
+// console.log(typeof(nAsNumber));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(process.env.ENVIRONMENT_MESSAGE)
 
 
